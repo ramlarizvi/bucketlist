65f3ec42504ba6bc40a48bbe1b11df276cf1652b
Ramla <110214759+ramlarizvi@users.noreply.github.com>
2024-01-03 19:44:54 +0530

    Add files via upload

---
+++ webss.js
@@ -0,0 +1,8 @@
+// get the div element
+const navbar = document.querySelector('#myDiv');
+
+// add a click event listener to the div
+navbar.addEventListener('click', function() 
+  // specify the action to take when the div is clicked
+ 
+);
